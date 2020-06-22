d692865c50125ff8366640de4765ed00756aec2c
Shivam Saini <shivamsn97@gmail.com>
2020-06-23 00:06:09 +0530

    Initial Release

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f: 
+    long_description = f.read() 
+
+setup(
+    name='smartinput',
+    version='0.1.0',
+    description='Much better implementation of the python input function, with hints and history support.',
+    license="MIT",
+    long_description=long_description,
+    author='Shivam Saini',
+    author_email='shivamsn97@gmail.com',
+    url='https://github.com/shivamsn97/smartinput',
+    packages=['smartinput'],
+    install_requires=['colorama','getch']
+)
