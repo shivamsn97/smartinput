184cf4b88094d662a4391abdc9a4b2c521f86fbd
Shivam Saini <shivamsn97@gmail.com>
2020-06-23 13:55:05 +0530

    Readme example fix.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='smartinput',
-    version='1.0.4',
+    version='1.0.5',
     python_requires='>=3',
     description='Much better implementation of the python input function, with hints and history support.',
     license="MIT",
