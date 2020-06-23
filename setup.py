15111ecf085e562e050fb057d41abdfc1889a472
Shivam Saini <shivamsn97@gmail.com>
2020-06-23 09:24:32 +0530

    Added python version classifier. v0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name='smartinput',
-    version='0.1.5',
+    version='0.1.6',
     python_requires='>=3',
     description='Much better implementation of the python input function, with hints and history support.',
     license="MIT",
@@ -15,5 +15,9 @@ setup(
     author_email='shivamsn97@gmail.com',
     url='https://github.com/shivamsn97/smartinput',
     packages=['smartinput'],
-    install_requires=['colorama==0.4.3','getch==1.0']
+    install_requires=['colorama==0.4.3','getch==1.0'],
+    classifiers=[ 'Programming Language :: Python :: 3',
+                  'Programming Language :: Python :: 3.5', 
+                  'Programming Language :: Python :: 3.6'
+                ]
 )
