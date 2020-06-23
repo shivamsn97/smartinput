699435cb7720c87d24dc381d596c9e3ebfb3f6b5
Shivam Saini <51438830+shivamsn97@users.noreply.github.com>
2020-06-23 18:52:31 +0530

    fix for windows

--- setup.py
+++ setup.py
@@ -6,20 +6,42 @@ if sys.version_info < (3,0):
 with open("README.md", 'r') as f: 
     long_description = f.read() 
 
-setup(
-    name='smartinput',
-    version='1.0.5',
-    python_requires='>=3',
-    description='Much better implementation of the python input function, with hints and history support.',
-    license="MIT",
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Shivam Saini',
-    author_email='shivamsn97@gmail.com',
-    url='https://github.com/shivamsn97/smartinput',
-    packages=['smartinput'],
-    install_requires=['colorama==0.4.3','getch==1.0'],
-    classifiers=[ 'Programming Language :: Python :: 3',
+import os
+if os.name == 'nt':
+    setup(
+        name='smartinput',
+        version='1.0.5',
+        python_requires='>=3',
+        description='Much better implementation of the python input function, with hints and history support.',
+        license="MIT",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Shivam Saini',
+        author_email='shivamsn97@gmail.com',
+        url='https://github.com/shivamsn97/smartinput',
+        packages=['smartinput'],
+        install_requires=['colorama==0.4.3'],
+        classifiers=[ 'Programming Language :: Python :: 3',
+                  'Programming Language :: Python :: 3.5', 
+                  'Programming Language :: Python :: 3.6',
+                  'Programming Language :: Python :: 3.7',
+                  'Programming Language :: Python :: 3.8'
+                ]
+else:   
+    setup(
+        name='smartinput',
+        version='1.0.5',
+        python_requires='>=3',
+        description='Much better implementation of the python input function, with hints and history support.',
+        license="MIT",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Shivam Saini',
+        author_email='shivamsn97@gmail.com',
+        url='https://github.com/shivamsn97/smartinput',
+        packages=['smartinput'],
+        install_requires=['colorama==0.4.3','getch==1.0'],
+        classifiers=[ 'Programming Language :: Python :: 3',
                   'Programming Language :: Python :: 3.5', 
                   'Programming Language :: Python :: 3.6',
                   'Programming Language :: Python :: 3.7',
