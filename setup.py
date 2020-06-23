d6519b316eb3a3c3e1854992aaff4a89adda8c50
Shivam Saini <shivamsn97@gmail.com>
2020-06-23 08:47:32 +0530

    Markdown Support added for long_description

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ with open("README.md", 'r') as f:
 
 setup(
     name='smartinput',
-    version='0.1.2',
+    version='0.1.3',
     description='Much better implementation of the python input function, with hints and history support.',
     license="MIT",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Shivam Saini',
     author_email='shivamsn97@gmail.com',
     url='https://github.com/shivamsn97/smartinput',
