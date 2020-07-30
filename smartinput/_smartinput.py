426e1498384ae5d3a332ed2f526b7d9559bcaed1
Shivam <shivamsn97@gmail.com>
2020-07-30 15:59:03 +0530

    try 3

--- smartinput/_smartinput.py
+++ smartinput/_smartinput.py
@@ -57,7 +57,7 @@ class Keys:
                     except IndexError:
                         return Keys.NONE
                 return Keys.NONE
-            return x
+            return str(x)
 
 
 x = Keys()
@@ -110,56 +110,53 @@ def suggestion(query, history, hints):
     
 def sinput(what = "",history=None, hints=None, historyAsHint=True,autohistory= True, eof=None, whatcolor=None, color=None, hintcolor=Fore.MAGENTA):
     print((whatcolor if whatcolor else "") + what + (Style.RESET_ALL if whatcolor else "") + (color if color else ""), end='', flush=True)
-    x = str(mygetch())
-    rtn = ""
+    keys = Keys()
+    x = keys.get()
+    rtn = "" 
     mhistory = [""] + history.aslist() if history else []
     hcur = 0
     rval = 0
     suggest = ""
     while(x != '\n' and x != '\r'):
-        if(ord(x)==4):
+        if(x == Keys.EOF):
             print(Style.RESET_ALL)
             if eof:
                 return eof
             raise EOFError
-        if(ord(x) == 127):
+        if(x == Keys.BACK):
             if(rtn):
                 rtn = (rtn[:-rval-1] if rval else rtn[:-1]) + (rtn[-rval:] if rval else "")
                 print('\b' + rtn[-rval:] +' ' + ' '*(len(suggest)) + '\b'*(rval+1+len(suggest)) if rval else " "*(len(suggest)) + '\b'*len(suggest) + "\b \b", end="", flush=True) 
-        elif(x== '\t'):
+        elif(x== Keys.TAB):
             rtn+=suggest
             print(rtn[-rval-len(suggest):] if (rval+len(suggest)) else "", end="", flush=True)
             rval=0
-        elif ord(x) == 27:
-            x = mygetch()
-            if (ord(x)==91):
-            #67 = right, 68 = left, 65= up, 66= down
-                x = ord(mygetch())
-                if(x == 68 and rval<len(rtn)):
-                    print('\b', end="", flush=True)
-                    rval +=1
-                if(x==67):
-                    if(rval):
-                        print(rtn[-rval], end="", flush=True)
-                        rval-=1
-                    else:
-                        x = "\t"
-                        continue
-                if history:
-                    if(x==66 and hcur>0):
-                        mhistory[hcur] = rtn
-                        hcur-=1
-                        print('\b'*(len(rtn)-rval)  + ' '*len(rtn) + '\b'*len(rtn), end="", flush=True)
-                        rtn = mhistory[hcur]
-                        print(rtn, end="", flush=True)
-                        rval=0
-                    elif(x== 65 and hcur < len(mhistory)-1):
-                        mhistory[hcur] = rtn
-                        hcur+=1
-                        print('\b'*(len(rtn)-rval) + ' '*len(rtn) + '\b'*len(rtn),end="", flush=True)
-                        rtn = mhistory[hcur]
-                        print(rtn, end="", flush=True)
-                        rval=0
+        elif(x == Keys.LEFT and rval<len(rtn)):
+            print('\b', end="", flush=True)
+            rval +=1
+        elif(x==Keys.RIGHT):
+            if(rval):
+                print(rtn[-rval], end="", flush=True)
+                rval-=1
+            else:
+                x = Keys.TAB
+                continue
+        elif(x==Keys.DOWN and hcur>0):
+            if history:
+                mhistory[hcur] = rtn
+                hcur-=1
+                print('\b'*(len(rtn)-rval)  + ' '*len(rtn) + '\b'*len(rtn), end="", flush=True)
+                rtn = mhistory[hcur]
+                print(rtn, end="", flush=True)
+                rval=0
+        elif(x== Keys.UP and hcur < len(mhistory)-1):
+            if history:
+                mhistory[hcur] = rtn
+                hcur+=1
+                print('\b'*(len(rtn)-rval) + ' '*len(rtn) + '\b'*len(rtn),end="", flush=True)
+                rtn = mhistory[hcur]
+                print(rtn, end="", flush=True)
+                rval=0
         else:
             rtn = (rtn[:-rval] if rval else rtn) + x + (rtn[-rval:] if rval else "")
             print(rtn[-rval-1:] + '\b'*rval if rval else x, end="", flush=True)
